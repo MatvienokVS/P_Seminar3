b4bdbb885407e723eb0acd060f8430a53d07d50b
MatvienokVS <skavaava@gmail.com>
2023-03-22 21:49:34 +0300

    Решение задач семинар 3

--- Ex2.py
+++ Ex2.py
@@ -1,11 +1,20 @@
 # Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X.
 # Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
 # В последующих строках записаны N целых чисел Ai. Последняя строка содержит число X
+
 from random import randint as rnd
 
 value_list = int(input('Введите количество элементов в массиве: '))
-search_numb = int(input('Введите искомое число Х: '))
+numb_list = [rnd(0, 5) for i in range(int(value_list))]
+search_numb = list(map(int, numb_list))
 
-numb_list = [rnd(0, 10) for i in range(value_list)]
-
+X = int(input('Введите искомое число Х: '))
+min = X - search_numb[0]
+index = 0
+for i in range(1, value_list):
+    count = X - search_numb[i]
+    if count < min:
+        min = count
+        index = i
 print(*numb_list)
+print(f'Число {search_numb[index]} в списке наиболее близко по величине к числу {X}')
